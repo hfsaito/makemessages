405857dd4f37e5e8bc3cfbb9355eef9772eba6bd
Hiroshi Farias Saito <hiroshi.saito@jupiter.lan>
2018-08-20 11:56:53 +0100

    Implemented reading config.json

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # makemessages
 Extract and compile messages to localize a web app
+
+This package intends to work simliar to django's makemessages and compilemessages commands
+
+## Installation
+```
+npm i makemessages
+npm i -g makemessages
+```
+
+## Usage
+if you install locally, change your package.json:
+```javascript
+{
+  // ...
+  "scripts": {
+    "makemessages": "makemessages",
+    // ...
+  }
+  // ...
+}
+```
+```
+npm run makemessages -- -c "./makemessages.json"
+```
+if you install globally:
+```
+makemessages -c "./makemessages.json"
+```
+
+## Configuration
+makemessages.json example
+```javascript
+{
+  "watch": "./test/samples/**/*.js", // Files that strings will be extracted using gettext function
+  "po": {
+    "languages": {
+      "en": "English",
+      "pt": "Portuguese",
+      "pt-br": "Brazilian Portuguese"
+    },
+    "output": "./path/to/output/po/"
+  },
+  "json": {
+    "input": "./path/to/output/po/",
+    "output": "./path/to/output/json/"
+  }
+}
+```
+Expected output
+```
+./path/to/output/
+  po/
+    en/
+      locale.po
+    pt/
+      locale.po
+    pt-br/
+      locale.po
+  json/
+    en/
+      locale.json
+    pt/
+      locale.json
+    pt-br/
+      locale.json
+```
+
+### Next features
+* Able to chagne transalation function through makemessage.json
+* Build PO file header
+* Able to chagne PO file header through makemessage.json
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
 import * as cli from 'cli';
+import * as fs from 'fs';
+import * as glob from 'glob';
+import * as path from 'path';
 import { Gatherer } from './Gatherer';
 import { POCompiler } from './Compiler/index';
 
+function absolutePath(file) { return path.resolve(process.cwd(), file); }
 
-// const [,, ...rawargs] = process.argv.reduce((acc, arg) => Object.assign(acc, { arg.split('=')[0]: arg.split('=')[1] }), {});
-// console.log(rawargs);
 let args = cli.parse({
-	config: [ 'c', 'A json file with your configurations', 'file', './makemessages.json' ] // ,          // -f, --file FILE   A file to process
-	// time: [ 't', 'An access time', 'time', false],                 // -t, --time TIME   An access time
-	// work: [ false, 'What kind of work to do', 'string', 'sleep' ]  //     --work STRING What kind of work to do
+	config: [ 'c', 'A json file with your configurations', 'file', './makemessages.json' ]
 });
-import(args.config).catch(err => cli.error(err));
-console.log(args);
 
-let g = new Gatherer();
-g.po('./test/samples/locale.po', './test/samples/**/*.js', 'gettext');
+async function main() {
 
-let c = new POCompiler();
-c.po2json('./test/samples_toignore/q.po', './test/samples_toignore/q.json');
+	const config = await import(absolutePath(args.config)).catch(console.error);
+	if (!config)
+		return;
+
+	let g = new Gatherer();
+	Object.keys(config.po.languages).forEach(lang => {
+
+		try {
+			fs.statSync(absolutePath(config.po.output) + `/${lang}/`);
+		} catch(e) {
+			fs.mkdirSync(absolutePath(config.po.output) + `/${lang}/`);
+		}
+		g.po(absolutePath(config.po.output) + `/${lang}/locale.po`, absolutePath(config.watch), 'gettext')
+	});
+	
+	const c = new POCompiler();
+	let inputOutputDict = {};
+	glob.sync(absolutePath(config.json.input) + '/**/*.po').forEach(file => {
+
+		inputOutputDict[file] = file.replace(absolutePath(config.json.input), absolutePath(config.json.output)).replace(/\.po$/g, '.json');
+	});
+	Object.keys(inputOutputDict).forEach(input => {
+	
+		try {
+			fs.statSync(path.dirname(inputOutputDict[input]));
+		} catch(e) {
+			fs.mkdirSync(path.dirname(inputOutputDict[input]));
+		}
+		c.po2json(input, inputOutputDict[input])
+	});
+}
+
+main();
